775fd1ccb8a3f57e4845b265e4838da5f090b6e6
cpandi <chinnavairan04@gmail.com>
2022-01-10 14:54:24 +0530

    Load Balanced and circuit breaker code added

--- Home-microservice1/src/main/java/com/coder/main/controller/HomeController.java
+++ Home-microservice1/src/main/java/com/coder/main/controller/HomeController.java
@@ -5,18 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class HomeController {
 
 	@Autowired
 	RestTemplate restTemplate;
-	
+
 	@GetMapping("/home")
+	@HystrixCommand(fallbackMethod = "handleProductServiceDown")
 	public String home() {
-		//return "Home Controller";
-		//String product = restTemplate.getForObject("http://DESKTOP-GBG5C18:Product-microservice:8082", String.class);
-		
-		String product = restTemplate.getForObject("http://localhost:8082/product", String.class);
+		// return "Home Controller";
+
+		// String product = restTemplate.getForObject("http://localhost:8082/product",
+		// String.class);
+		String product = restTemplate.getForObject("http://PRODUCT-MICROSERVICE/product", String.class);
+		System.out.println("Home::::" + product);
 		return product;
 	}
+
+	public String handleProductServiceDown() {
+		return "called as fallback method";
+	}
+
 }
--- Product1-microservice/src/main/java/com/coder/main/ServletInitializer.java
+++ Product1-microservice/src/main/java/com/coder/main/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Product1MicroserviceApplication.class);
+		return application.sources(ProductMicroserviceApplication.class);
 	}
 
 }
